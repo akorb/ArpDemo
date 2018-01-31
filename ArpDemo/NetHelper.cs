7ff1a3776d820d095c31e8be670586d4410a7c0f
akorb <andreas.d.korb@gmail.com>
2018-01-31 21:14:02 +0100

    Refactoring

--- ArpDemo/NetHelper.cs
+++ ArpDemo/NetHelper.cs
@@ -8,108 +8,106 @@ using System.Net.NetworkInformation;
 
 namespace ArpDemo
 {
-	public static class NetHelper
-	{
-		public static String GetMacAddress(LivePacketDevice device, String targetAddress)
-		{
-			NetworkInterface currentInterface = NetworkInterface.GetAllNetworkInterfaces().First(x => x.OperationalStatus == OperationalStatus.Up && x.NetworkInterfaceType != NetworkInterfaceType.Loopback);
-			byte[] mac = currentInterface.GetPhysicalAddress().GetAddressBytes();
-			byte[] ipSender = currentInterface.GetIPProperties().UnicastAddresses.First(x => x.PrefixLength == 24).Address.GetAddressBytes();
-			byte[] ipTarget = targetAddress.Split('.').Select(byte.Parse).ToArray();
+    public static class NetHelper
+    {
+        public static String GetMacAddress(LivePacketDevice device, String targetAddress)
+        {
+            NetworkInterface currentInterface = NetworkInterface.GetAllNetworkInterfaces().First(x => x.OperationalStatus == OperationalStatus.Up && x.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+            byte[] mac = currentInterface.GetPhysicalAddress().GetAddressBytes();
+            byte[] ipSender = currentInterface.GetIPProperties().UnicastAddresses.First(x => x.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).Address.GetAddressBytes();
+            byte[] ipTarget = targetAddress.Split('.').Select(byte.Parse).ToArray();
 
-			byte[] arpData = new byte[28];
-			// HRD
-			arpData[1] = 1;
-			// PRO
-			arpData[2] = 8;
-			arpData[3] = 0;
-			// HLN
-			arpData[4] = 6;
-			// PLN
-			arpData[5] = 4;
-			// OP
-			arpData[6] = 0;
-			arpData[7] = 1;
-			// SHA
-			arpData[8] = mac[0];
-			arpData[9] = mac[1];
-			arpData[10] = mac[2];
-			arpData[11] = mac[3];
-			arpData[12] = mac[4];
-			arpData[13] = mac[5];
-			// SPA
-			arpData[14] = ipSender[0];
-			arpData[15] = ipSender[1];
-			arpData[16] = ipSender[2];
-			arpData[17] = ipSender[3];
-			// THA
-			arpData[18] = byte.MaxValue;
-			arpData[19] = byte.MaxValue;
-			arpData[20] = byte.MaxValue;
-			arpData[21] = byte.MaxValue;
-			arpData[22] = byte.MaxValue;
-			arpData[23] = byte.MaxValue;
-			// TPA
-			arpData[24] = ipTarget[0];
-			arpData[25] = ipTarget[1];
-			arpData[26] = ipTarget[2];
-			arpData[27] = ipTarget[3];
-
-
-			byte[] ethernetData = new byte[14];
-			// Destination MAC address
-			ethernetData[0] = byte.MaxValue;
-			ethernetData[1] = byte.MaxValue;
-			ethernetData[2] = byte.MaxValue;
-			ethernetData[3] = byte.MaxValue;
-			ethernetData[4] = byte.MaxValue;
-			ethernetData[5] = byte.MaxValue;
-			//Source MAC address
-			ethernetData[6] = mac[0];
-			ethernetData[7] = mac[1];
-			ethernetData[8] = mac[2];
-			ethernetData[9] = mac[3];
-			ethernetData[10] = mac[4];
-			ethernetData[11] = mac[5];
-			// Type
-			ethernetData[12] = 8;
-			ethernetData[13] = 6;
-
-			var fullPacketData = new List<byte>();
-			fullPacketData.AddRange(ethernetData);
-			fullPacketData.AddRange(arpData);
-
-			String macAsText = currentInterface.GetPhysicalAddress().ToString();
-			for (int i = macAsText.Length - 2; i >= 2; i -= 2)
-				macAsText = macAsText.Insert(i, ":");
+            byte[] arpData = new byte[28];
+            // HRD
+            arpData[1] = 1;
+            // PRO
+            arpData[2] = 8;
+            arpData[3] = 0;
+            // HLN
+            arpData[4] = 6;
+            // PLN
+            arpData[5] = 4;
+            // OP
+            arpData[6] = 0;
+            arpData[7] = 1;
+            // SHA
+            arpData[8] = mac[0];
+            arpData[9] = mac[1];
+            arpData[10] = mac[2];
+            arpData[11] = mac[3];
+            arpData[12] = mac[4];
+            arpData[13] = mac[5];
+            // SPA
+            arpData[14] = ipSender[0];
+            arpData[15] = ipSender[1];
+            arpData[16] = ipSender[2];
+            arpData[17] = ipSender[3];
+            // THA
+            arpData[18] = byte.MaxValue;
+            arpData[19] = byte.MaxValue;
+            arpData[20] = byte.MaxValue;
+            arpData[21] = byte.MaxValue;
+            arpData[22] = byte.MaxValue;
+            arpData[23] = byte.MaxValue;
+            // TPA
+            arpData[24] = ipTarget[0];
+            arpData[25] = ipTarget[1];
+            arpData[26] = ipTarget[2];
+            arpData[27] = ipTarget[3];
 
 
-			Packet pkt = new Packet(fullPacketData.ToArray(), DateTime.Now, DataLinkKind.Ethernet);
+            byte[] ethernetData = new byte[14];
+            // Destination MAC address
+            ethernetData[0] = byte.MaxValue;
+            ethernetData[1] = byte.MaxValue;
+            ethernetData[2] = byte.MaxValue;
+            ethernetData[3] = byte.MaxValue;
+            ethernetData[4] = byte.MaxValue;
+            ethernetData[5] = byte.MaxValue;
+            //Source MAC address
+            ethernetData[6] = mac[0];
+            ethernetData[7] = mac[1];
+            ethernetData[8] = mac[2];
+            ethernetData[9] = mac[3];
+            ethernetData[10] = mac[4];
+            ethernetData[11] = mac[5];
+            // Type
+            ethernetData[12] = 8;
+            ethernetData[13] = 6;
 
-			PacketCommunicator connection = device.Open(65536, PacketDeviceOpenAttributes.None, 100);
-			connection.SetFilter("arp and ether dst " + macAsText + " and src host " + targetAddress);
-			connection.SendPacket(pkt);
+            var fullPacketData = new List<byte>();
+            fullPacketData.AddRange(ethernetData);
+            fullPacketData.AddRange(arpData);
 
-			Stopwatch watch = Stopwatch.StartNew();
+            string macAsText = currentInterface.GetPhysicalAddress().ToString();
+            for (int i = macAsText.Length - 2; i >= 2; i -= 2)
+                macAsText = macAsText.Insert(i, ":");
 
-			// Retrieve the packets
-			do
-			{
-				Packet packet;
-				PacketCommunicatorReceiveResult result = connection.ReceivePacket(out packet);
-				switch (result)
-				{
-					case PacketCommunicatorReceiveResult.Timeout:
-						break;
-					case PacketCommunicatorReceiveResult.Ok:
-						return packet.Ethernet.Source.ToString();
-					default:
-						throw new InvalidOperationException("The result " + result + " should never be reached here");
-				}
-			}
-			while (watch.ElapsedMilliseconds < 5000);
 
-			return "Not found";
-		}
-	}
+            Packet pkt = new Packet(fullPacketData.ToArray(), DateTime.Now, DataLinkKind.Ethernet);
+
+            PacketCommunicator connection = device.Open(65536, PacketDeviceOpenAttributes.None, 100);
+            connection.SetFilter("arp and ether dst " + macAsText + " and src host " + targetAddress);
+            connection.SendPacket(pkt);
+
+            Stopwatch watch = Stopwatch.StartNew();
+            // Retrieve the packets
+            do
+            {
+                PacketCommunicatorReceiveResult result = connection.ReceivePacket(out Packet packet);
+                switch (result)
+                {
+                    case PacketCommunicatorReceiveResult.Timeout:
+                        break;
+                    case PacketCommunicatorReceiveResult.Ok:
+                        return packet.Ethernet.Source.ToString();
+                    default:
+                        throw new InvalidOperationException("The result " + result + " should never be reached here");
+                }
+            }
+            while (watch.ElapsedMilliseconds < 5000);
+
+            return "Not found";
+        }
+    }
 }
--- ArpDemo/Program.cs
+++ ArpDemo/Program.cs
@@ -3,42 +3,42 @@ using System;
 
 namespace ArpDemo
 {
-	class Program
-	{
-		static void Main()
-		{
-			LivePacketDevice device = GetActivePacketDevice();
+    class Program
+    {
+        static void Main()
+        {
+            LivePacketDevice device = GetActivePacketDevice();
 
-			Console.Clear();
+            Console.Clear();
 
-			Console.Write("IP Adresse: ");
-			String ipTargetInput = Console.ReadLine();
+            Console.Write("IP Adresse: ");
+            string ipTargetInput = Console.ReadLine();
+            string macTarget = NetHelper.GetMacAddress(device, ipTargetInput);
 
-			Console.WriteLine(NetHelper.GetMacAddress(device, ipTargetInput));
+            Console.WriteLine(macTarget);
+            Console.ReadLine();
+        }
 
-			Console.ReadLine();
-		}
+        static LivePacketDevice GetActivePacketDevice()
+        {
+            if (LivePacketDevice.AllLocalMachine.Count == 1)
+                return LivePacketDevice.AllLocalMachine[0];
 
-		static LivePacketDevice GetActivePacketDevice()
-		{
-			if (LivePacketDevice.AllLocalMachine.Count == 1)
-				return LivePacketDevice.AllLocalMachine[0];
+            string input;
+            int result;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < LivePacketDevice.AllLocalMachine.Count; i++)
+                {
+                    Console.WriteLine("[" + (i + 1) + "]: " + LivePacketDevice.AllLocalMachine[i].Description);
+                }
 
-			String input;
-			int result;
-			do
-			{
-				Console.Clear();
-				for (int i = 0; i < LivePacketDevice.AllLocalMachine.Count; i++)
-				{
-					Console.WriteLine("[" + (i + 1) + "]: " + LivePacketDevice.AllLocalMachine[i].Description);
-				}
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out result) || --result >= LivePacketDevice.AllLocalMachine.Count);
 
-				input = Console.ReadLine();
-			}
-			while (!int.TryParse(input, out result) || --result >= LivePacketDevice.AllLocalMachine.Count);
-
-			return LivePacketDevice.AllLocalMachine[result];
-		}
-	}
+            return LivePacketDevice.AllLocalMachine[result];
+        }
+    }
 }
